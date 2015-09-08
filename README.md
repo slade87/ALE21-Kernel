e57accb6bd61588662062e4dd95c842414e36796
Slade87 <antonh@gmx.de>
2015-09-08 20:03:32 +0200

    Initial commit for ALE-21 Kernel:
    ALE_kernel_[Android 5.0 EMUI3.0]
    
    Kernel is 3.10.61
    
    The Kernelsource was modified to be able to compile it all
    since there were quite some issues with includes and LD setup.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ALE21-Kernel
+Use ARCH=arm64 make hisi_hi6210sft_defconfig
+ARCH=arm64 CROSS_COMPILE={Your aarch64 toolchain} make -j6
---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Sep 8 19:52:34 CEST 2015"
+#define LINUX_COMPILE_BY "android"
+#define LINUX_COMPILE_HOST "localhost"
+#define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (Linaro GCC 4.9-2015.01-3) "
